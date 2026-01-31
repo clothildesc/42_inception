1dbc38b1ba91ba1998467f1a28355ef8403082d2
Clothilde Scache <156417371+clothildesc@users.noreply.github.com>
2026-01-31 16:23:17 +0100

    add comments in Dockerfile and fixed issues in bonus services

--- srcs/requirements/bonus/backup/tools/backup.sh
+++ srcs/requirements/bonus/backup/tools/backup.sh
@@ -3,5 +3,11 @@ set -e
 
 export MYSQL_PASSWORD=$(cat /run/secrets/db_password)
 
-mysqldump -h mariadb -u wp_user -p"$MYSQL_PASSWORD" wordpress_db \
+# Creation de copie de la base WP horodate pour eviter ecrasement
+mysqldump \
+  -h "$WP_DB_HOST" \
+  -P "${WP_DB_PORT:-3306}" \
+  -u "$WP_DB_USER" \
+  -p"$MYSQL_PASSWORD" \
+  "$WP_DB_NAME" \
   > /backups/db_$(date +%F_%H-%M).sql
--- srcs/requirements/bonus/ftp/tools/ftp-setup.sh
+++ srcs/requirements/bonus/ftp/tools/ftp-setup.sh
@@ -6,4 +6,6 @@ FTP_PASSWORD=$(cat /run/secrets/ftp_password)
 
 echo "$FTP_USER:$FTP_PASSWORD" | chpasswd
 
+chown -R ftpuser:ftpuser /var/www/html
+
 exec "$@"
